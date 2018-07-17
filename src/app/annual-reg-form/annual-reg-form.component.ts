714826d9fb6a4ea24c156ad2cc6715f47b5987d2
in2smithereens <in2smithereens@gmail.com>
2018-07-17 11:59:52 -0700

    Experimenting with EventListener
    Trying to calculate registration costs dynamically

--- src/app/annual-reg-form/annual-reg-form.component.ts
+++ src/app/annual-reg-form/annual-reg-form.component.ts
@@ -7,9 +7,60 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AnnualRegFormComponent implements OnInit {
 
-  constructor() { }
+  constructor() { 
+    var btn = document.getElementById("calcRegBtn");
+    if(btn)
+      btn.addEventListener("onclick", this.calcCosts);
+  }
 
   ngOnInit() {
   }
 
+  calcNumChildren() {
+    console.log("calcNumChildren() called");
+   // var strChild1 = document.getElementById('childname');
+    var totChildren = 5;
+    var strChild1 = document.forms['reg']['childname1'].value;
+    var strChild2 = document.forms['reg']['childname2'].value;
+    var strChild3 = document.forms['reg']['childname3'].value;
+    var strChild4 = document.forms['reg']['childname4'].value;
+    var strChild5 = document.forms['reg']['childname5'].value;
+  
+    if (strChild5 == null || strChild5 == "")
+    {
+      totChildren--;
+    }
+    if (strChild4 == null || strChild4 == "")
+    {
+      totChildren--;
+    }
+    if (strChild3 == null || strChild3 == "")
+    {
+      totChildren--;
+    }
+    if (strChild2 == null || strChild2 == "")
+    {
+      totChildren--;
+    }
+    if (strChild1 == null || strChild1 == "")
+    {
+      totChildren--;
+    }
+    console.log("totChildren = " + totChildren);
+    document.getElementById("numChildren").innerHTML = totChildren.toString();
+    return totChildren;
+  }
+  
+  calcCosts() {
+    console.log("calcCosts() called");
+    var children = this.calcNumChildren();
+    var regFees = 0;
+    while (children > 0) {
+      regFees += 95;
+      children--;
+    }
+    document.getElementById("regtotal").innerHTML = regFees.toString();
+    return null;
+  }
+    
 }
